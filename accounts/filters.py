1cfe4d0081d64fcfcf02d3a383939ecf54627970
JohnGuzman <jlg05@hotmail.co.uk>
2023-02-18 23:39:01 +0000

    first commit

---
+++ accounts/filters.py
@@ -0,0 +1,14 @@
+import django_filters
+from .models import *
+from django_filters import DateFilter, CharFilter
+
+class orderFilter(django_filters.FilterSet):
+    star_date = DateFilter(field_name='date_created', lookup_expr='gte')
+    end_date = DateFilter(field_name='date_created', lookup_expr='lte')
+    note = CharFilter( field_name='note', lookup_expr='icontains')
+
+
+    class Meta:
+        model =  Order
+        fields = '__all__'
+        exclude = ['customer', 'date_created']
